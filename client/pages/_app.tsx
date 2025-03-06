316fbe4d173673c79a6777f16eca18b8e43acb38
jvdjugal3886 <89655571+jvdjugal3886@users.noreply.github.com>
2025-03-06 09:20:25 +0530

    Initial commit

---
+++ client/pages/_app.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+
+import Cssbaseline from '@mui/material/CssBaseline'
+import type { AppProps } from 'next/app'
+
+import { FirebaseProvider } from 'context/FirebaseContext'
+import { MediaStreamProvider } from 'context/MediaStream'
+import { MediaScreenStreamProvider } from 'context/ScreenStream'
+import { SocketProvider } from 'context/SocketContext'
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps | any) {
+  return (
+    <FirebaseProvider>
+      <SocketProvider>
+        <MediaScreenStreamProvider>
+          <MediaStreamProvider>
+            <Cssbaseline />
+            <Head>
+              <title>WorkItOut</title>
+              <link
+                rel="icon"
+                href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"
+              />
+              <meta
+                name="viewport"
+                content="initial-scale=1.0, width=device-width"
+              />
+            </Head>
+            <Component {...pageProps} />
+          </MediaStreamProvider>
+        </MediaScreenStreamProvider>
+      </SocketProvider>
+    </FirebaseProvider>
+  )
+}
+
+export default MyApp
